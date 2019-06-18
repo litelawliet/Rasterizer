e14bfb118dd4a784caded90a2cd6be75ed0152bc
Thibaut PONCHON <thibo_thiba@hotmail.fr>
2019-06-18 18:07:29 -0400

    openGl context set

--- Rasterizer/Rasterizer/src/main.cpp
+++ Rasterizer/Rasterizer/src/main.cpp
@@ -1,24 +1,39 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::ContextSettings settings;
 
-    while (window.isOpen())
-    {
-        sf::Event event{};
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	settings.majorVersion = 4;
+	settings.minorVersion = 5;
+	settings.antialiasingLevel = 16;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!", sf::Style::Default, settings);
+	window.setVerticalSyncEnabled(true);
 
-    return 0;
+	std::cout << "depth bits:" << settings.depthBits << std::endl;
+	std::cout << "stencil bits:" << settings.stencilBits << std::endl;
+	std::cout << "antialiasing level:" << settings.antialiasingLevel << std::endl;
+	std::cout << "version:" << settings.majorVersion << "." << settings.minorVersion << std::endl;
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
